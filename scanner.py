861562375d01eed4f254332e9964de2534b322fe
drewsuitor <drew@suitor.dev>
2020-04-04 16:42:16 -0400

    threshold final barcode to only black/white

--- scanner.py
+++ scanner.py
@@ -53,8 +53,10 @@ class Scanner:
 
         # crop and return just the barcode
         box_img = self.image[y1:y2, x1:x2]
+        box_img = cv2.cvtColor(box_img, cv2.COLOR_BGR2GRAY)
+        _, box_thresh = cv2.threshold(box_img, 127, 255, 0)
 
-        return box_img
+        return box_thresh
 
 
 if __name__ == '__main__':
